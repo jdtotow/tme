e8de6819516ac27b81f3e2c5827fbe100fb2fe00
Jean-Didier <ttjd2005@yahoo.fr>
2020-03-16 16:43:50 +0200

    auto deploy

--- kubernetes/configs/prepare-all.sh
+++ kubernetes/configs/prepare-all.sh
@@ -6,6 +6,7 @@ kubectl apply -f deployment/mongodb-volume.yaml
 kubectl apply -f deployment/prometheus-config-volume.yaml 
 kubectl apply -f deployment/sidecar-volume-prometheus.yaml 
 kubectl apply -f deployment/minio-volume.yaml 
+kubectl apply -f deployment/prometheus-ks8-volume.yaml 
 
 #create pvc
 kubectl apply -f deployment/prometheus-config-volume-claim.yaml 
@@ -13,6 +14,7 @@ kubectl apply -f deployment/prometheus-tsdb-volume-claim.yaml
 kubectl apply -f deployment/mongodb-volume-claim.yaml
 kubectl apply -f deployment/sidecar-volume-prometheus-claim.yaml
 kubectl apply -f deployment/minio-volume-claim.yaml 
+kubectl apply -f deployment/prometheus-ks8-volume-claim.yaml 
 
 kubectl create configmap configmap-prometheus --from-file=../../prometheus/prometheus.yml
 kubectl create configmap configmap-manager --from-file=../../manager/config.json
@@ -20,12 +22,23 @@ kubectl create configmap configmap-qos --from-file=../../qos/config/config.json
 kubectl create configmap configmap-pdp --from-file=../../pdp/config/config.json 
 kubectl create configmap configmap-bucket --from-file=../../thanos/thanos/bucket_config.yaml 
 kubectl create configmap prometheus-cluster --from-file=../../cluster/prometheus/prometheus.yml 
-#sleep 20
-#kubectl apply -f deployment/prometheus.yml 
-#kubectl apply -f deployment/grafana.yml 
-#kubectl apply -f deployment/prometheusbeat.yml 
-#kubectl apply -f deployment/manager.yml 
-#kubectl apply -f deployment/exporter.yml 
-#kubectl apply -f deployment/mongodb.yml 
-#kubectl apply -f deployment/qos.yml 
-#kubectl apply -f deployment/pdp.yml 
+sleep 5
+kubectl apply -f deployment/mongodb.yaml 
+kubectl apply -f deployment/minio.yaml 
+kubectl apply -f deployment/rabbitmq.yaml 
+kubectl apply -f deployment/prometheus.yaml 
+sleep 20
+kubectl apply -f deployment/sidecar.yaml
+sleep 30
+kubectl apply -f deployment/querier.yaml 
+kubectl apply -f deployment/compactor.yaml 
+kubectl apply -f deployment/gateway.yaml 
+sleep 20  
+kubectl apply -f deployment/prometheusbeat.yaml 
+kubectl apply -f deployment/manager.yaml 
+sleep 30
+kubectl apply -f deployment/exporter.yaml 
+kubectl apply -f deployment/qos.yaml 
+kubectl apply -f deployment/pdp.yml 
+kubectl apply -f deployment/grafana.yaml
+kubectl apply -f deployment/ml.yaml 
