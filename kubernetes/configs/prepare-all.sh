9f2ed05b6f122565ebea0c9a91dc63f839092be6
jdtotow <ttjd2005@yahoo.fr>
2020-04-27 17:00:19 +0300

    changes prometheus kube

--- kubernetes/configs/prepare-all.sh
+++ kubernetes/configs/prepare-all.sh
@@ -20,21 +20,21 @@ kubectl apply -f deployment/prometheus-ks8-volume-claim.yaml
 kubectl apply -f deployment/volume-manager-claim.yaml 
 kubectl apply -f deployment/volume-pdp-claim.yaml 
 
-kubectl create configmap configmap-prometheus --from-file=../../prometheus/prometheus.yml
+kubectl create configmap configmap-prometheus --from-file=../../prometheus/prometheus-k8s.yml
 kubectl create configmap configmap-bucket --from-file=../../thanos/thanos/bucket_config.yaml 
 kubectl create configmap prometheus-cluster --from-file=../../cluster/prometheus/prometheus.yml 
 kubectl create configmap configmap-logstash --from-file=../../logstash/logstash-2.conf 
-sleep 5
-kubectl apply -f deployment/mongodb.yaml 
-kubectl apply -f deployment/minio.yaml 
-kubectl apply -f deployment/rabbitmq.yaml 
-kubectl apply -f deployment/prometheus.yaml 
-sleep 20
-kubectl apply -f deployment/sidecar.yaml
-sleep 30
-kubectl apply -f deployment/querier.yaml 
-kubectl apply -f deployment/compactor.yaml 
-kubectl apply -f deployment/gateway.yaml 
+#sleep 5
+#kubectl apply -f deployment/mongodb.yaml 
+#kubectl apply -f deployment/minio.yaml 
+#kubectl apply -f deployment/rabbitmq.yaml 
+#kubectl apply -f deployment/prometheus.yaml 
+#sleep 20
+#kubectl apply -f deployment/sidecar.yaml
+#sleep 30
+#kubectl apply -f deployment/querier.yaml 
+#kubectl apply -f deployment/compactor.yaml 
+#kubectl apply -f deployment/gateway.yaml 
 #sleep 20  
 #kubectl apply -f deployment/prometheusbeat.yaml 
 #kubectl apply -f deployment/manager.yaml 
